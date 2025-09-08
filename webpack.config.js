efe0f74c36c70d7308e62af3cfbfe49edcd672b8
TanPhan1112 <minhtan111293@gmail.com>
2025-09-08 22:17:21 +0700

    Updated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,36 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-mode:"production",
-devtool:"source-map",
+  mode: "production",
+  devtool: "source-map",
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
-    module: {
-        rules: [
-            {
-              test:/\.css$/,
-              use:['style-loader', 'css-loader']
-            },
-            { test: /\.m?js$/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['@babel/preset-env', '@babel/preset-react' ]
-                }
-              }
-            }
-
-        ]
-    }
-
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
 };
